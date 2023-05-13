704705cca3f379ec991249d8c57f8d9317400688
lephuthuc2001 <lephuthuc2001@gmail.com>
2023-05-13 15:24:05 +0700

    Added Title and Imported Google Fonts

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="font-gluten absolute top-8 text-center xl:text-6xl md:text-5xl  text-4xl w-full">
+      Todolist
+    </div>
+  );
+}
+
+export default Title;
